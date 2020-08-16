8a3d31c02ada7c6ff24976e6ebf90888cd18a221
Bryan <bcaraway@cern.ch>
2020-08-16 17:39:00 -0500

    fixed setup step

--- DeepSleep/setup.sh
+++ DeepSleep/setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Setup file system for analysis
 
-mkdir files
+mkdir -p files
 
 YEARS=(2016 2017 2018)
 for y in ${YEARS[@]}; do
-    mkdir files/$y 
-    mkdir files/$y/mc_files
-    mkdir files/$y/data_files
+    mkdir -p files/$y 
+    mkdir -p files/$y/mc_files
+    mkdir -p files/$y/data_files
 done
 
-mkdir pdf 
-mkdir log
+mkdir -p pdf 
+mkdir -p log
 #mkdir NN_files
 #cp /cms/data/store/user/ttxeft/NN_files/* NN_files/
